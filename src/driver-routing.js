9c128f6e7fb33d0791052e3401d1ad4eb41e2a1c
mlpierce22 <mlpierce22@gmail.com>
2020-08-23 00:13:39 -0700

    add driver routing.js file

--- src/driver-routing.js
+++ src/driver-routing.js
@@ -1,3 +1,33 @@
-export function setupAPIKey(apiKey) {
-  console.log("woo! Setup the api key!: ", apiKey);
+/* eslint-disable @typescript-eslint/camelcase */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const openrouteservice = require("openrouteservice-js");
+
+let directions;
+let geocode;
+let matrix;
+let coordinates;
+function setupAPIKey(apiKey) {
+  console.log("registering api key!");
+  directions = new openrouteservice.Directions({
+    api_key: apiKey
+  });
+  geocode = new openrouteservice.Geocode({
+    api_key: apiKey
+  });
+  matrix = new openrouteservice.Matrix({
+    api_key: apiKey
+  });
+  console.log("API key has successfully been applied");
+}
+
+export function toCoordinates(data) {
+  console.log("to coordinates");
+  data.stores.forEach(store => {
+    console.log(store);
+  });
+}
+
+export function calculateRoute(data) {
+  setupAPIKey(data["api-key"]);
+  toCoordinates(data);
 }
